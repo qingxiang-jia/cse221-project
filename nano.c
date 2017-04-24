4485279cab84c286092ebaa51bd3ad0723254874
Qingxiang Jia <ybjqx3340@gmail.com>
2017-04-24 16:27:41 -0700

    added code for linux

--- nano.c
+++ nano.c
@@ -5,15 +5,46 @@
 #include <mach/mach_time.h>
 
 static mach_timebase_info_data_t timebaseInfo;
-static void __attribute__((constructor)) getTimebase() {
+static void __attribute__((constructor)) getTimebase()
+{
   mach_timebase_info(&timebaseInfo);
 }
 
-uint64_t getNano() {
+uint64_t getNano()
+{
   uint64_t time = mach_absolute_time();
   time = time * timebaseInfo.numer;
   time = time / timebaseInfo.denom;
   return time;
+}
+
+#else
+static uint64_t ticks = 0;
+
+static inline uint64_t rdtsc()
+{
+  uint32_t hiReg, loReg;
+  asm volatile("rdtscp\n"
+               "movl %%edx, %0\n"
+               "movl %%eax, %1\n"
+               "cpuid"
+               : "=r"(hiReg), "=r"(loReg)
+               :
+               : "%rax", "%rbx", "%rcx", "%rdx");
+  return (((uint64_t)hiReg) << 32) | (uint64_t)loReg;
+}
+
+static __attribute__((constructor)) ticksPerMicroSec()
+{
+  uint64_t start = rdtsc();
+  usleep(1); //1 microsecond
+  ticks = rdtsc() - start;
+}
+
+uint64_t getNano()
+{
+  printf("ubuntu\n");
+  return (double)rdtsc() / (double)ticks * 1000;
 }
 
 #endif
