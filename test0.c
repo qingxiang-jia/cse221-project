e907065c0b28d52b31e59332d4c67a56230ce209
Qingxiang Jia <ybjqx3340@gmail.com>
2017-04-25 11:31:43 -0700

    okay, implemented according to the intel paper

--- test0.c
+++ test0.c
@@ -1,17 +1,25 @@
 #include <stdio.h>
 #include <unistd.h>
-#include "nano.h"
 
 int main()
 {
-  int i;
-  uint64_t a;
-  uint64_t b;
-  for (i = 0; i < 10; i++) {
-    a = getNano();
-    sleep(1);
-    b = getNano();
-    printf("%llu\n", b-a);
-  }
+  uint64_t start, end;
+  unsigned cycles_high, cycles_low, cycles_high1, cycles_low1;
+  asm volatile (
+    "CPUID\n\t"
+    "RDTSC\n\t"
+    "mov %%edx, %0\n\t"
+    "mov %%eax, %1\n\t": "=r" (cycles_high), "=r" (cycles_low)::"%rax", "%rbx", "%rcx", "%rdx"
+  );
+  // sleep(1);
+  asm volatile (
+    "RDTSCP\n\t"
+    "mov %%edx, %0\n\t"
+    "mov %%eax, %1\n\t"
+    "CPUID\n\t": "=r" (cycles_high1), "=r" (cycles_low1)::"%rax", "%rbx", "%rcx", "%rdx"
+  );
+  start = ((uint64_t)cycles_high << 32 | cycles_low);
+  end = ((uint64_t)cycles_high1 << 32 | cycles_low1);
+  printf("%llu\n", end - start);
   return 0;
 }
