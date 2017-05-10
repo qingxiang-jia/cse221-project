ad03d35459b659e60b9d6888bb11ab953abeed04
Qingxiang Jia <ybjqx3340@gmail.com>
2017-05-10 16:41:55 -0700

    off to CSE 291

--- op2.c
+++ op2.c
@@ -4,7 +4,36 @@
 #include <stdlib.h>
 #include "count.h"
 
+/* Measure RAM Access Time BEGIN */
+
+void expRAMAccessTime(int increment, int arraySize) {
+  int i;
+  int j;
+  int *ptr = malloc(sizeof(int) * arraySize);
+
+  for (i = 0; i < arraySize; i += increment) {
+    double start, end;
+    unsigned lo, hi, lo1, hi1;
+    COUNT1(hi, lo)
+    j = *ptr;
+    COUNT2(hi1, lo1)
+    GETNUM(hi, lo, start)
+    GETNUM(hi1, lo1, end)
+    ptr += increment;
+    printf("%f\n", end - start);
+  }
+}
+
+void measureRAMAccessTime() {
+  int increment = 1;
+  int arraySize = 2;
+  for (; increment < )
+}
+
+/* Measure RAM Access Time END */
+
 int main()
 {
+  expRAMAccessTime(2, 10);
   return 0;
 }
