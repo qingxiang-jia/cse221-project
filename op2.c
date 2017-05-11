81eb961c86214065e802f1cb3a0a774d6939bbf4
Qingxiang Jia <ybjqx3340@gmail.com>
2017-05-11 13:33:39 -0700

    memory access time seems to be working

--- op2.c
+++ op2.c
@@ -1,15 +1,26 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <unistd.h>
+#include <time.h>
 #include <stdlib.h>
 #include "count.h"
 
 /* Measure RAM Access Time BEGIN */
 
+#define SZ_INT 4
+
 void expRAMAccessTime(int increment, int arraySize) {
   int i;
   int j;
-  int *ptr = malloc(sizeof(int) * arraySize);
+  int *chunkPtr = malloc(SZ_INT * arraySize);
+  int *ptr = chunkPtr;
+  
+  if (chunkPtr == NULL) {
+    printf("malloc failed");
+    exit(1);
+  }
+  double avg = 0;
+  double count = 0;
 
   for (i = 0; i < arraySize; i += increment) {
     double start, end;
@@ -19,21 +30,33 @@ void expRAMAccessTime(int increment, int arraySize) {
     COUNT2(hi1, lo1)
     GETNUM(hi, lo, start)
     GETNUM(hi1, lo1, end)
-    ptr += increment;
-    printf("%f\n", end - start);
+    // ptr += increment;
+    ptr = chunkPtr + (rand() % arraySize);
+    avg += (end - start);
+    count++;
   }
+  printf("avg: %f\n", avg / count);
+  free(chunkPtr);
 }
 
 void measureRAMAccessTime() {
-  int increment = 1;
-  int arraySize = 2;
-  for (; increment < )
+  int increment;
+  int maxIncrement = 10;
+  int arraySize;
+  int maxArraySize = 16777216; // 2^24
+  // for (increment = 1; increment <= maxIncrement; increment++) {
+    for (arraySize = 2; arraySize <= maxArraySize; arraySize *= 2) {
+      printf("increment: %d  arraySize: %d\n", increment, arraySize);
+      expRAMAccessTime(increment, arraySize);
+    }
+  // }
 }
 
 /* Measure RAM Access Time END */
 
 int main()
 {
-  expRAMAccessTime(2, 10);
+  srand(time(NULL));
+  measureRAMAccessTime();
   return 0;
 }
