4f2125f2d349544c258881c9cf966c272facd565
Qingxiang Jia <ybjqx3340@gmail.com>
2017-05-25 17:35:43 -0700

    finished file cache size test

--- fs.c
+++ fs.c
@@ -10,7 +10,6 @@
 /* Measure File Cache Size BEGIN */
 
 #define SZ_30G 32212254720
-
 #define SZ_19G 20401094656
 #define SZ_18G 19327352832
 #define SZ_17G 18253611008
@@ -30,39 +29,58 @@
 #define SZ_3G 3221225472
 #define SZ_2G 2147483648
 #define SZ_1G 1073741824
+#define SZ_10MB 10485760
+#define SZ_1MB 1048576
 #define SSD_BLOCK_SIZE 4096
+#define CYC_PER_SECOND 2.4e9
 void measureFileCacheSize(uint64_t fileSize, char *path)
 {
   int largeFile = open(path, O_RDONLY);
-  char buf[SSD_BLOCK_SIZE]; // 1 char = 1 byte
+  char *buf = malloc(SZ_10MB); // 1 char = 1 byte
   uint64_t numOfChunks = fileSize / SSD_BLOCK_SIZE;
   uint64_t i;
 
+  uint64_t k;
   double start, end;
   unsigned lo, hi, lo1, hi1;
   COUNT1(hi, lo)
   for (i = 0; i < numOfChunks; i++)
   {
-    read(largeFile, buf, SSD_BLOCK_SIZE);
+    k = read(largeFile, buf, SSD_BLOCK_SIZE);
   }
   COUNT2(hi1, lo1)
   GETNUM(hi, lo, start)
   GETNUM(hi1, lo1, end)
-  printf("%f\n", end - start);
+  double rate = (fileSize / 1024.0 / 1024.0) / ((end - start) / CYC_PER_SECOND);
+  // printf("%f MB/s\n", rate);
+  close(largeFile);
 
-  int exp;
-  for (exp = 0; exp < 10; exp++)
+  int round;
+  ssize_t bytesRead;
+  uint64_t offset = 1024 * SSD_BLOCK_SIZE;
+  uint64_t readOccurrence = 0;
+  for (round = 0; round < 1; round++)
   {
+    largeFile = open(path, O_RDONLY);
     start = 0, end = 0, hi = 0, lo = 0, hi1 = 0, lo1 = 0;
     COUNT1(hi, lo)
-    for (i = 0; i < numOfChunks; i++)
+    while (1)
     {
-      read(largeFile, buf, SSD_BLOCK_SIZE);
+      bytesRead = read(largeFile, buf, SZ_10MB);
+      if (bytesRead <= 0)
+      {
+        break;
+      }
+      lseek(largeFile, offset, SEEK_CUR);
+      readOccurrence++;
     }
     COUNT2(hi1, lo1)
     GETNUM(hi, lo, start)
     GETNUM(hi1, lo1, end)
-    printf("%f\n", end - start);
+    rate = ((double) (SZ_10MB * readOccurrence) / 1024.0 / 1024.0) / ((end - start) / CYC_PER_SECOND);
+    // printf("read %zd time\n", readOccurrence);
+    printf("%f MB/s\n", rate);
+    close(largeFile);
   }
 }
 
@@ -70,28 +88,28 @@ int main()
 {
   system("sudo purge");
   measureFileCacheSize(SZ_1G, "/Users/lee/Documents/1g");
-  printf("\n");
+  // printf("\n");
   system("sudo purge");
   measureFileCacheSize(SZ_2G, "/Users/lee/Documents/2g");
-  printf("\n");
+  // printf("\n");
   system("sudo purge");
   measureFileCacheSize(SZ_3G, "/Users/lee/Documents/3g");
-  printf("\n");
+  // printf("\n");
   system("sudo purge");
   measureFileCacheSize(SZ_4G, "/Users/lee/Documents/4g");
-  printf("\n");
+  // printf("\n");
   system("sudo purge");
   measureFileCacheSize(SZ_5G, "/Users/lee/Documents/5g");
-  printf("\n");
+  // printf("\n");
   system("sudo purge");
   measureFileCacheSize(SZ_6G, "/Users/lee/Documents/6g");
-  printf("\n");
+  // printf("\n");
   system("sudo purge");
   measureFileCacheSize(SZ_7G, "/Users/lee/Documents/7g");
-  printf("\n");
+  // printf("\n");
   system("sudo purge");
   measureFileCacheSize(SZ_8G, "/Users/lee/Documents/8g");
-  printf("\n");
+  // printf("\n");
 
   return 0;
 }
---
+++ tests/fs/RunFS.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+echo "2"
+../../fs
+
+echo "3"
+../../fs
+
+echo "4"
+../../fs
+
+echo "5"
+../../fs
+
+echo "6"
+../../fs
+
+echo "7"
+../../fs
+
+echo "8"
+../../fs
+
+echo "9"
+../../fs
