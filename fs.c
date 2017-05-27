8669305c092c994e65ef4e0f5247e6008601e6de
Qingxiang Jia <ybjqx3340@gmail.com>
2017-05-27 01:03:11 -0700

    contention done

--- fs.c
+++ fs.c
@@ -86,7 +86,7 @@ void measureFileCacheSize(uint64_t fileSize, char *path)
     COUNT2(hi1, lo1)
     GETNUM(hi, lo, start)
     GETNUM(hi1, lo1, end)
-    rate = ((double) (SZ_10MB * readOccurrence) / 1024.0 / 1024.0) / ((end - start) / CYC_PER_SECOND);
+    rate = ((double)(SZ_10MB * readOccurrence) / 1024.0 / 1024.0) / ((end - start) / CYC_PER_SECOND);
     // printf("read %zd time\n", readOccurrence);
     printf("%f MB/s\n", rate);
     close(largeFile);
@@ -140,21 +140,23 @@ void readFileNoCacheSeq(uint64_t fileSize, char *path)
 
   double rate;
   double perBlockTime;
-  for (i = 0; i < 10; i++) {
+  for (i = 0; i < 10; i++)
+  {
     system("sudo purge");
     file = open(path, O_RDONLY);
     fcntl(file, F_NOCACHE, 1);
     uint64_t j;
     start = 0, end = 0, lo = 0, hi = 0, lo1 = 0, hi1 = 0;
     COUNT1(hi, lo)
-    for (j = 0; j < numOfBlocks; j++) {
+    for (j = 0; j < numOfBlocks; j++)
+    {
       read(file, buf, SSD_BLOCK_SIZE);
     }
     COUNT2(hi1, lo1)
     GETNUM(hi, lo, start)
     GETNUM(hi1, lo1, end)
     rate = (fileSize / 1024.0 / 1024.0) / ((end - start) / CYC_PER_SECOND);
-    perBlockTime = (end - start) / (double) numOfBlocks / CYC_PER_SECOND * 1000;
+    perBlockTime = (end - start) / (double)numOfBlocks / CYC_PER_SECOND * 1000;
     printf("%-10f MB/s           %-10f ms per block\n", rate, perBlockTime);
     close(file);
   }
@@ -173,31 +175,34 @@ void readFileNoCacheRand(uint64_t fileSize, char *path)
   unsigned lo, hi, lo1, hi1;
 
   time_t t;
-  srand((unsigned) time(&t));
+  srand((unsigned)time(&t));
   double rate;
   double perBlockTime;
-  for (i = 0; i < 10; i++) {
+  for (i = 0; i < 10; i++)
+  {
     system("sudo purge");
     file = open(path, O_RDONLY);
     fcntl(file, F_NOCACHE, 1);
     uint64_t iterations = numOfBlocks / 2;
     off_t *offset = malloc(iterations * sizeof(off_t));
     uint64_t j;
-    for (j = 0; j < iterations; j++) {
-      offset[j] = (off_t) (rand() % (fileSize - SSD_BLOCK_SIZE));
+    for (j = 0; j < iterations; j++)
+    {
+      offset[j] = (off_t)(rand() % (fileSize - SSD_BLOCK_SIZE));
     }
 
     start = 0, end = 0, lo = 0, hi = 0, lo1 = 0, hi1 = 0;
     COUNT1(hi, lo)
-    for (j = 0; j < iterations; j++) {
+    for (j = 0; j < iterations; j++)
+    {
       lseek(file, offset[j], SEEK_SET);
       read(file, buf, SSD_BLOCK_SIZE);
     }
     COUNT2(hi1, lo1)
     GETNUM(hi, lo, start)
     GETNUM(hi1, lo1, end)
-    rate = ((double) iterations * (double) SSD_BLOCK_SIZE / 1024.0 / 1024.0) / ((end - start) / CYC_PER_SECOND);
-    perBlockTime = (end - start) / (double) iterations / CYC_PER_SECOND * 1000;
+    rate = ((double)iterations * (double)SSD_BLOCK_SIZE / 1024.0 / 1024.0) / ((end - start) / CYC_PER_SECOND);
+    perBlockTime = (end - start) / (double)iterations / CYC_PER_SECOND * 1000;
     printf("%-10f MB/s           %-10f ms per block\n", rate, perBlockTime);
     close(file);
     free(offset);
@@ -205,7 +210,8 @@ void readFileNoCacheRand(uint64_t fileSize, char *path)
   free(buf);
 }
 
-int main() {
+int main2()
+{
   // printf("1MB\n");
   // readFileNoCacheSeq(SZ_1MB, "/Users/lee/Documents/1m");
   // printf("2MB\n");
@@ -228,29 +234,108 @@ int main() {
   // readFileNoCacheSeq(SZ_512MB, "/Users/lee/Documents/512m");
   // printf("1G\n");
   // readFileNoCacheSeq(SZ_1G, "/Users/lee/Documents/1g");
-  printf("1MB\n");
-  readFileNoCacheRand(SZ_1MB, "/Users/lee/Documents/1m");
-  printf("2MB\n");
-  readFileNoCacheRand(SZ_2MB, "/Users/lee/Documents/2m");
-  printf("4MB\n");
-  readFileNoCacheRand(SZ_4MB, "/Users/lee/Documents/4m");
-  printf("8MB\n");
-  readFileNoCacheRand(SZ_8MB, "/Users/lee/Documents/8m");
-  printf("16MB\n");
-  readFileNoCacheRand(SZ_16MB, "/Users/lee/Documents/16m");
-  printf("32MB\n");
-  readFileNoCacheRand(SZ_32MB, "/Users/lee/Documents/32m");
-  printf("64MB\n");
-  readFileNoCacheRand(SZ_64MB, "/Users/lee/Documents/64m");
-  printf("128MB\n");
-  readFileNoCacheRand(SZ_128MB, "/Users/lee/Documents/128m");
-  printf("256MB\n");
-  readFileNoCacheRand(SZ_256MB, "/Users/lee/Documents/256m");
-  printf("512MB\n");
-  readFileNoCacheRand(SZ_512MB, "/Users/lee/Documents/512m");
-  printf("1G\n");
-  readFileNoCacheRand(SZ_1G, "/Users/lee/Documents/1g");
+  // printf("1MB\n");
+  // readFileNoCacheRand(SZ_1MB, "/Users/lee/Documents/1m");
+  // printf("2MB\n");
+  // readFileNoCacheRand(SZ_2MB, "/Users/lee/Documents/2m");
+  // printf("4MB\n");
+  // readFileNoCacheRand(SZ_4MB, "/Users/lee/Documents/4m");
+  // printf("8MB\n");
+  // readFileNoCacheRand(SZ_8MB, "/Users/lee/Documents/8m");
+  // printf("16MB\n");
+  // readFileNoCacheRand(SZ_16MB, "/Users/lee/Documents/16m");
+  // printf("32MB\n");
+  // readFileNoCacheRand(SZ_32MB, "/Users/lee/Documents/32m");
+  // printf("64MB\n");
+  // readFileNoCacheRand(SZ_64MB, "/Users/lee/Documents/64m");
+  // printf("128MB\n");
+  // readFileNoCacheRand(SZ_128MB, "/Users/lee/Documents/128m");
+  // printf("256MB\n");
+  // readFileNoCacheRand(SZ_256MB, "/Users/lee/Documents/256m");
+  // printf("512MB\n");
+  // readFileNoCacheRand(SZ_512MB, "/Users/lee/Documents/512m");
+  // printf("1G\n");
+  // readFileNoCacheRand(SZ_1G, "/Users/lee/Documents/1g");
   return 0;
 }
 
 /* Measure File Read Time END */
+
+/* Measure Contention BEGIN */
+
+void readFileSeq(uint64_t fileSize, char *path)
+{
+  int file = open(path, O_RDONLY);
+  fcntl(file, F_NOCACHE, 1);
+  char *buf = malloc(SZ_10MB); // 1 char = 1 byte
+  uint64_t numOfBlocks = fileSize / SSD_BLOCK_SIZE;
+  uint64_t i;
+
+  double start, end;
+  unsigned lo, hi, lo1, hi1;
+
+  double rate;
+  double perBlockTime;
+  for (i = 0; i < 10; i++)
+  {
+    file = open(path, O_RDONLY);
+    fcntl(file, F_NOCACHE, 1);
+    uint64_t j;
+    start = 0, end = 0, lo = 0, hi = 0, lo1 = 0, hi1 = 0;
+    COUNT1(hi, lo)
+    for (j = 0; j < numOfBlocks; j++)
+    {
+      read(file, buf, SSD_BLOCK_SIZE);
+    }
+    COUNT2(hi1, lo1)
+    GETNUM(hi, lo, start)
+    GETNUM(hi1, lo1, end)
+    rate = (fileSize / 1024.0 / 1024.0) / ((end - start) / CYC_PER_SECOND);
+    perBlockTime = (end - start) / (double)numOfBlocks / CYC_PER_SECOND * 1000;
+    printf("%-10f MB/s           %-10f ms per block\n", rate, perBlockTime);
+    close(file);
+  }
+  free(buf);
+}
+
+void spawn(int numOfProcesses, char *pathTemplate) // pathTemplate: /Users/lee/Documents/512m%d
+{
+  int i, pid;
+  char filePath[50];
+  system("sudo purge");
+
+  for (i = 1; i <= numOfProcesses; i++)
+  {
+    pid = fork();
+    if (pid == -1)
+    {
+      printf("Spawning failed.");
+      return;
+    }
+    if (pid == 0)
+    {
+      sprintf(filePath, pathTemplate, i);
+      // printf("filePath: %s\n", filePath);
+      readFileSeq(SZ_64MB, filePath);
+      printf("Process %d is done.\n", i);
+      return;
+    }
+  }
+}
+
+int main(int argc, char *argv[])
+{
+  printf("\n\n");
+  spawn(atoi(argv[1]), "/Users/lee/Documents/64m%d");
+  // if (argc < 2)
+  // {
+  //   spawn(2);
+  // }
+  // else
+  // {
+  //   spawn(atoi(argv[1]));
+  // }
+  return 0;
+}
+
+/* Measure Contention END */
